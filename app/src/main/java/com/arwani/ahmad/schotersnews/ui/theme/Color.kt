3a143e7025665f0798f28b67f3743d456baaf734
arwani <arwaniahmad659@gmail.com>
2023-01-01 22:55:12 +0700

    add logo

--- app/src/main/java/com/arwani/ahmad/schotersnews/ui/theme/Color.kt
+++ app/src/main/java/com/arwani/ahmad/schotersnews/ui/theme/Color.kt
@@ -2,7 +2,16 @@ package com.arwani.ahmad.schotersnews.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val Black100 = Color(0xFF2C2C2E)
+val Black200 = Color(0xFF191918)
+val White = Color(0XFFFFFFFF)
+val Yellow100 = Color(0XFFF0CAA3)
+val Red200 = Color(0XFFFF0566)
+val Blue50 = Color(0XFFCFF5E7)
+val Blue100 = Color(0xFF89C4E1)
+val Blue200 = Color(0xFF0082FF)
+val Blue500 = Color(0xFF7900FF)
+val Grey100 = Color(0xFF6E6F71)
+val Green100 = Color(0XFF25D366)
+val Green200 = Color(0XFFB3FFAE)
--- app/src/main/java/com/arwani/ahmad/schotersnews/ui/theme/Theme.kt
+++ app/src/main/java/com/arwani/ahmad/schotersnews/ui/theme/Theme.kt
@@ -6,25 +6,19 @@ import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
+
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Black200,
+    primaryVariant = Black100,
+    secondary = White,
+    surface = Grey100,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = Black200,
+    primaryVariant = Black100,
+    secondary = White,
+    surface = Grey100,
 )
 
 @Composable
